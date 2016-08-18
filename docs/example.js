3e6b44495b00027c2a6488ab37e61df3da2a12ab
Dean McDonnell <mcdonnelldean@outlook.com>
2016-08-18 11:15:05 +0100

    Updated stuff

---
+++ docs/example.js
@@ -0,0 +1,12 @@
+require('seneca')
+    .use('parambulator') // not needed if Seneca 2.x
+    .add(
+      {
+        a: 1,
+        b: {required$: true}
+      },
+      function (msg, done) {
+        done(null, {c: msg.b})
+      })
+    .act('a:1,b:2') // valid
+    .act('a:1') // invalid as no b value
